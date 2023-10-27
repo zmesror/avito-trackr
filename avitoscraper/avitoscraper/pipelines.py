577951bd70e401cd0ed6423c4a4fc8d94b807c32
Zakaria Mesror <zames066@gmail.com>
2023-10-27 14:46:28 +0200

    Add item pipelines

--- avitoscraper/avitoscraper/pipelines.py
+++ avitoscraper/avitoscraper/pipelines.py
@@ -6,8 +6,17 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+from datetime import datetime
 
 class AvitoscraperPipeline:
     def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+
+        # Convert published_date to datetime
+        date = adapter["published_date"]
+        adapter["published_date"] = self._to_datetime(date)
+
         return item
+    
+    def _to_datetime(self, datetime_str):
+        return datetime.strptime(datetime_str, "%Y-%m-%dT%H:%M:%SZ")
